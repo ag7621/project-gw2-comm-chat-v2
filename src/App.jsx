4e1c522e423891d009ef2815cef9e531c7352c1e
ag7621 <aaron.c.guerra@gmail.com>
2025-05-17 17:06:08 -0700

    added filter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
   const [data, setData] = useState(bossList);
   const [active, setActive] = useState('tab-1');
 
+  const [selectedWing, setSelectedWing] = useState(1);
+  const filteredList = data.filter((boss) => boss.wing === selectedWing);
+
   function handleAddEntry(selectedBoss, newEntry) {
     setData((prevData) =>
       prevData.map((boss) =>
@@ -23,6 +26,8 @@ function App() {
   }
 
   function handleActiveTab(e) {
+    setSelectedWing(Number(e.target.value));
+    console.log(filteredList);
     setActive(e.target.id);
   }
 
@@ -39,16 +44,16 @@ function App() {
         <h1>GW2 Chat</h1>
         {/* <Navbar onFilterChange={handleFilter} /> */}
         <div className="tab-list">
-          <button id="tab-1" onClick={handleActiveTab}>
+          <button id="tab-1" onClick={handleActiveTab} value={1}>
             Tab 1
           </button>
-          <button id="tab-2" onClick={handleActiveTab}>
+          <button id="tab-2" onClick={handleActiveTab} value={2}>
             Tab 2
           </button>
-          <button id="tab-3" onClick={handleActiveTab}>
+          <button id="tab-3" onClick={handleActiveTab} value={3}>
             Tab 3
           </button>
-          <button id="tab-4" onClick={handleActiveTab}>
+          <button id="tab-4" onClick={handleActiveTab} value={4}>
             Tab 4
           </button>
         </div>
@@ -61,10 +66,23 @@ function App() {
           <List wing={wing} />
         ))} */}
         <div className="tab-content">
-          <div className={`tab-page ${active === 'tab-1' ? 'active' : ''}`}>
+          {/* <div className={`tab-page ${active === 'tab-1' ? 'active' : ''}`}>
             <p>tab 1 content</p>
+            {filteredList.map((item) => (
+              <h2>{item.name}</h2>
+            ))}
+          </div> */}
+          <div>
+            {filteredList.map((item) => (
+              <>
+                <h3>{item.name}</h3>
+                {item.entries.map((entry) => (
+                  <p>{entry.entry}</p>
+                ))}
+              </>
+            ))}
           </div>
-          <div className={`tab-page ${active === 'tab-2' ? 'active' : ''}`}>
+          {/* <div className={`tab-page ${active === 'tab-2' ? 'active' : ''}`}>
             <p>tab 2 content</p>
           </div>
           <div className={`tab-page ${active === 'tab-3' ? 'active' : ''}`}>
@@ -72,7 +90,7 @@ function App() {
           </div>
           <div className={`tab-page ${active === 'tab-4' ? 'active' : ''}`}>
             <p>tab 4 content</p>
-          </div>
+          </div> */}
         </div>
       </main>
 
